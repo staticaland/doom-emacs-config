41930d139ac916be88ac1f2ee8d656ce5a155134
Anders Pettersen <anders.k.pettersen@protonmail.com>
2020-08-31 20:47:48 +0200

    Clean up

--- config.el
+++ config.el
@@ -1,6 +1,6 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;;;; Docs
+;;;; Doom things
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
@@ -11,22 +11,6 @@
 (setq user-full-name "Anders K. Pettersen"
       user-mail-address "john@doe.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'modus-operandi)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -39,28 +23,7 @@
 
 (setq evil-snipe-scope 'visible)
 
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-;;
-
-
-
-;;;; Config
+;;;; Visuals
 
 (setq doom-variable-pitch-font (font-spec :family "EtBembo" :size 18))
 (setq doom-themes-treemacs-enable-variable-pitch nil)
@@ -72,7 +35,6 @@
         which-key-idle-secondary-delay 0.01
         which-key-sort-order 'which-key-key-order-alpha))
 
-(setq org-babel-python-command "python3")
 
 (when (string= (system-name) "Anderss-MacBook-Pro.local")
   (setq doom-font (font-spec :family "Menlo" :size 15)
@@ -89,6 +51,28 @@
   :config
     (treemacs-git-mode 'extended)
     (add-to-list 'treemacs-pre-file-insert-predicates #'treemacs-is-file-git-ignored?))
+
+;;;; Outline
+
+(use-package bicycle
+    :after outline)
+
+(map! :map outline-minor-mode-map
+    :n "<tab>" #'bicycle-cycle
+    :n "<backtab>" #'bicycle-cycle-global)
+
+(use-package prog-mode
+  :config
+  (add-hook 'prog-mode-hook 'outline-minor-mode)
+  (add-hook 'prog-mode-hook 'hs-minor-mode))
+
+
+(use-package! outline-minor-faces
+  :after outline
+  :config (add-hook 'outline-minor-mode-hook
+                    'outline-minor-faces-add-font-lock-keywords))
+
+;;;; File operations
 
 (after! dired
   (if (executable-find "gls")
@@ -110,7 +94,6 @@
   :hook ((dired-mode . dired-hide-details-mode)
          (dired-mode . hl-line-mode))
   :config
-  (setq delete-by-moving-to-trash t)
   (setq dired-listing-switches "-lat") ; sort by date (new first)
   (put 'dired-find-alternate-file 'disabled nil))
 
@@ -203,6 +186,7 @@
   (map! :leader
         (:prefix ("f" . "file")
          :desc "Open init.org" "o" '(lambda () (interactive) (find-file "~/org/org.org"))))
+  (setq org-babel-python-command "python3")
   (setq org-bullets-bullet-list '(" "))
   (setq org-cycle-separator-lines 1)
   (setq org-edit-src-content-indentation 0)
@@ -236,22 +220,3 @@
  (setq doom-modeline-persp-name t
        doom-modeline-persp-icon t))
 
-;;;; Outline
-
-(use-package bicycle
-    :after outline)
-
-(map! :map outline-minor-mode-map
-    :n "<tab>" #'bicycle-cycle
-    :n "<backtab>" #'bicycle-cycle-global)
-
-(use-package prog-mode
-  :config
-  (add-hook 'prog-mode-hook 'outline-minor-mode)
-  (add-hook 'prog-mode-hook 'hs-minor-mode))
-
-
-(use-package! outline-minor-faces
-  :after outline
-  :config (add-hook 'outline-minor-mode-hook
-                    'outline-minor-faces-add-font-lock-keywords))
