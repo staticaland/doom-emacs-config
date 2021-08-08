4826155c761a09baaf040ecc2f69204912c45543
Anders Pettersen <anders.k.pettersen@protonmail.com>
2021-08-08 19:39:45 +0200

    Bump reformatter

--- modules/editor/reformatter/packages.el
+++ modules/editor/reformatter/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/reformatter/packages.el
 
-(package! reformatter :pin "45c0add95025f53ca644a6c8b9afa05b2da3c474")
+(package! reformatter :pin "e02a9ea94287f4195edeeab3033e017a56872f5b")
