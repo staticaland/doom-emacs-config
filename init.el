e789de447181d3f5b19419b048dd6cdba408d4fc
Anders <anders@const.dev>
2022-04-09 11:07:38 +0200

    Update init.el

--- init.el
+++ init.el
@@ -8,8 +8,8 @@
 ;;;; Completion
 
        :completion
-       (company +childframe)
-       ivy
+       company
+       vertico
 
 ;;;; UI
 
@@ -23,24 +23,24 @@
        ;;indent-guides   ; highlighted indent columns
        modeline
        nav-flash         ; blink cursor line after big motions
-       ;;ophints           ; highlight the region an operation acts on
-       ;;(popup +defaults) ; tame sudden yet inevitable temporary windows
+       ;; ophints           ; highlight the region an operation acts on
+       ;; (popup +defaults) ; tame sudden yet inevitable temporary windows
        treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        workspaces        ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
-       ;;deft
+       ;; zen               ; distraction-free coding or writing
+       ;; deft
 
 ;;;; Editor
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
+       ;; file-templates    ; auto-snippets for empty files
        ;;fold              ; (nigh) universal code folding
        ;;lispy
-       reformatter
-       snippets          ; my elves. They type so I don't have to
+       ;; reformatter
+       ;; snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
 ;;;; Emacs
@@ -49,7 +49,7 @@
        (dired +icons)    ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ibuffer         ; interactive buffer management
-       ;;undo              ; persistent, smarter undo for your inevitable mistakes
+       ;; undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
 ;;;; Checkers
@@ -60,27 +60,27 @@
 ;;;; Tools
 
        :tools
-       ansible
-       docker
+       ;; ansible
+       ;; docker
        editorconfig      ; let someone else argue about tabs vs spaces
-       (eval +overlay)     ; run code, run (also, repls)
+       ;; (eval +overlay)     ; run code, run (also, repls)
        ;;lookup              ; navigate your code and its documentation
        lsp
        magit             ; a git porcelain for Emacs
        terraform         ; infrastructure as code
-       tmux              ; an API for interacting with tmux
+       ;; tmux              ; an API for interacting with tmux
 
 ;;;; Term
 
        :term
-       ;;vterm
+       ;; vterm
 
 ;;;; Lang
 
        :lang
        data              ; config/data formats
        emacs-lisp
-       (go +lsp)
+       ;; (go +lsp)
        json              ; At least it ain't XML
        markdown          ; writing docs for people to ignore
        (org +pretty +journal +hugo +roam +present)
@@ -100,7 +100,7 @@
 ;;;; App
 
        :app
-       (rss +org)        ; emacs as an RSS reader
+       ;; (rss +org)        ; emacs as an RSS reader
 
 ;;;; Config
 
