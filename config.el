4a6c38fcd54e6dc01bfe0381e1fc0533348e19ea
Anders K. Pettersen <staticaland@users.noreply.github.com>
2025-08-22 18:31:10 +0200

    feat: okay new method

--- config.el
+++ config.el
@@ -2,3 +2,6 @@
 
 (setq user-full-name "Anders K. Pettersen"
       user-mail-address "staticaland@users.noreply.github.com")
+
+(org-babel-load-file (expand-file-name "fonts.org" doom-user-dir))
+(org-babel-load-file (expand-file-name "gptel.org" doom-user-dir))
